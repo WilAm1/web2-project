fc8e7c00fea5cc40124e27d1cecb2d4473e4a06b
Isagani Lapira Jr <105891207+Isagani-lapira@users.noreply.github.com>
2023-03-24 20:43:17 +0800

    Update updateProcess.php

--- updateProcess.php
+++ updateProcess.php
@@ -1,12 +1,16 @@
 <?php
-
 $xml = new DOMDocument('1.0');
+$xml->load("BSIT3EG1G4.xml");
 
 $xml->formatOutput = true;
 $xml->preserveWhiteSpace = false;
 
-$xml->load("BSIT3EG1G4.xml");
-
+                
+$searchName = $_POST['name'];
+$year = $_POST['year'];
+$tagline = $_POST['tagline'];
+$branches = $_POST['branches'];
+$headquarter = $_POST['headquarter'];
 
 $companies = $xml->getElementsByTagName("techCompany");
 $flag = 0;
@@ -21,7 +25,7 @@ foreach ($companies as $company) {
         $yearElem = $xml->createElement("yearStart", $year);
         $taglineElem = $xml->createElement("tagline", $tagline);
         $branchElem = $xml->createElement("totalBranch", $branches);
-        $headquarterElem = $xml->createElement("headquarter", $headquarter);
+        $headquarterElem = $xml->createElement("headquarter",$headquarter);
 
 
         $newNode->appendChild($nameElem);
@@ -33,7 +37,23 @@ foreach ($companies as $company) {
         $xml->getElementsByTagName('techCompanies')->item(0)->replaceChild($newNode, $company);
         $xml->save("BSIT3EG1G4.xml");
 
-        echo "Details updated." . "<a href='index.php'>Back</a>";
+
+        echo '
+            <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
+            <link rel="stylesheet" href="BSIT3EG1G4.css" />
+
+            <div class="modalContainer p-4">
+                <div class="modalContent rounded bg-light p-3 mx-auto">
+                    <h2 class="text-success">Update Successful</h2>
+                    <hr>
+                    <p><span class="fw-bold">'.$name.'</span>is successfully updated!</p>
+
+                    <button class="d-block ms-auto btn btn-secondary">
+                        <a class="text-decoration-none" href="index.php">Close</a>
+                    </button>
+                </div>
+            </div>
+        ';
         break;
     }
 }
