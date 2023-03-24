3966be724e7c0f8106569da216f65b883d868ffd
Isagani Lapira Jr <105891207+Isagani-lapira@users.noreply.github.com>
2023-03-24 20:41:50 +0800

    Update deleteProcess.php

--- deleteProcess.php
+++ deleteProcess.php
@@ -1,11 +1,11 @@
 <?php
 
 $xml = new DOMDocument('1.0');
+$xml->load("BSIT3EG1G4.xml");
 
 $xml->formatOutput = true;
 $xml->preserveWhiteSpace = false;
 
-$xml->load("BSIT3EG1G4.xml");
 $searchName = $_POST['name'];
 
 $companies = $xml->getElementsByTagName("techCompany");
@@ -19,7 +19,28 @@ foreach ($companies as $company) {
         $flag = 1;
         $xml->getElementsByTagName('techCompanies')->item(0)->removeChild($company);
         $xml->save("BSIT3EG1G4.xml");
-        echo "Company Deleted. <a href='index.php'>Back</a>";
+       
+        echo '
+            <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
+            <link rel="stylesheet" href="BSIT3EG1G4.css" />
+
+            <div class="modalContainer p-4">
+                <div class="modalContent rounded bg-light p-3 mx-auto">
+                    <h2 class="text-success">Company deleted!</h2>
+                    <hr>
+                    <p><span class="fw-bold text-primary">'.$name.'</span>
+                    successfully deleted
+                    </p>
+
+                    <div class="d-flex justify-content-end">
+                        <button class= "btn btn-secondary">
+                            <a class=" text-decoration-none text-light" href="index.php">back</a>
+                        </button>
+                    </div>
+                    
+                </div>
+            </div>
+        ';
         break;
     }
 }
