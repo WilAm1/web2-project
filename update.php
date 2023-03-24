f44a056d6948c5e1d0e31628d02a5d4063639432
Isagani Lapira Jr <105891207+Isagani-lapira@users.noreply.github.com>
2023-03-24 20:42:28 +0800

    Update update.php

--- update.php
+++ update.php
@@ -1,55 +1,68 @@
 <!DOCTYPE html>
-
 <head>
-
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="BSIT3EG1G4.css" />
     <title>Update </title>
 </head>
 
-<body>
-    <h1>Update a Company</h1>
-    <form action="updateProcess.php" method="post">
-        <div>
-            Name:
-            <select name="name">
-                <?php
+<body class="bg-secondary">
+    <div class="container text-center">
+        <h1 class="text-light">Update a Company</h1>
+        <form action="updateProcess.php" method="post">
 
-                $xml = new DOMDocument("1.0");
-                $xml->load("BSIT3EG1G4.xml");
-                $companies = $xml->getElementsByTagName("techCompany");
+            <div class="formContainer">
 
-                foreach ($companies as $com) {
-                    $name = $com->getElementsByTagName("companyName")->item(0)->nodeValue;
-                    echo "<option value='$name' > $name</option>";
-                }
-                ?>
+                <!-- company name in dropdown -->
+                <div  class="input-group mb-2 w-75 mx-auto">
+                    <label class="input-group-text fw-bold" for="name">Name</label>
+                    <select class="form-select" name="name" id="name">
+                        <?php
 
-            </select>
-        </div>
-        <div>
-            Year:
-            <input type="number" name="year" required>
-        </div>
-        <div>
-            Tagline:
-            <input type="text" name="tagline" required>
-        </div>
-        <div>
-            Branches:
-            <input type="number" name="branches" required>
-        </div>
-        <div>
-            Headquarter:
-            <input type="text" name="headquarter" required>
-        </div>
+                        $xml = new DOMDocument("1.0");
+                        $xml->load("BSIT3EG1G4.xml");
+                        $companies = $xml->getElementsByTagName("techCompany");
+
+                        foreach ($companies as $com) {
+                            $name = $com->getElementsByTagName("companyName")->item(0)->nodeValue;
+                            echo "<option value='$name' > $name</option>";
+                        }
+                        ?>
+
+                    </select>
+                </div>
+
+                <!-- year -->
+                <div class="input-group mb-2 w-75 mx-auto">
+                    <label class="input-group-text fw-bold" for="year">Year</label>
+                    <input class="form-control" type="number" name="year" id="year" required>
+                </div>
+
+                <!-- tagline -->
+                <div class="input-group mb-2 w-75 mx-auto">
+                    <label class="input-group-text fw-bold" for="tagline">Tagline</label>
+                    <input class="form-control" type="text" name="tagline" id="tagline" required>
+                </div>
+
+                <!-- branches -->
+                <div class="input-group mb-2 w-75 mx-auto">
+                    <label class="input-group-text fw-bold" for="branches">Branches</label>
+                    <input class="form-control" type="number" name="branches" id="branches" required>
+                </div>
+
+                <div class="input-group mb-2 w-75 mx-auto">
+                    <label class="input-group-text fw-bold" for="headquarter">Headquarter</label>
+                    <input class="form-control" type="text" name="headquarter" id="headquarter" required>
+                </div>
 
 
-        <button>
-            <a href="index.php">
-                Back</a>
-        </button>
+                <button class="btn btn-outline-primary">
+                    <a href="index.php">Back</a>
+                </button>
 
-        <button>Save</button>
-    </form>
+                <button class="btn btn-primary">Save</button>
+            </div>
+        </form>
+    </div>
 
 
 
