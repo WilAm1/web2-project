ca6b42df7a7115613746f23378123a7417f98531
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-03-27 20:00:11 +0800

    refactor: Revert old table functionality to use native php instead of ajax

--- index.php
+++ index.php
@@ -107,31 +107,41 @@
     })
   </script>
 
-  <!-- show the data in table   -->
-  <div id="table-container"></div>
-  <script>
-    // handles fetching of data
-    var tableContainer = document.getElementById("table-container");
 
-    //  Function that will perform ajax to fetch data in the xml   
-    function fetchData() {
-      var http = new XMLHttpRequest();
-      http.onreadystatechange = function() {
+  <!-- handles the list of companies. -->
+  <?php
+  $xml = new DOMDocument("1.0");
+  $xml->load("./BSIT3EG1G4.xml");
 
-        if (http.readyState === 4 && http.status === 200) {
+  $companies = $xml->getElementsByTagName("techCompany");
 
-          tableContainer.innerHTML = http.responseText;
-          console.log("The table is refreshed.");
-        }
-      }
-      http.open("GET", 'fetchAllCompanies.php', true); //where we will request the data
-      http.send();
-    }
 
-    fetchData();
-    //refresh the table every 3 seconds
-    setInterval(fetchData, 3000);
-  </script>
+  echo '<table class="table w-75 ms-auto me-auto mt-5 table-dark table-striped ">';
+  echo '<tr class="bg-primary text-light"><th>Name</th>
+              <th>Year Started:</th>
+              <th>Tagline:</th>
+              <th>Branches:</th>
+              <th>Headquarters:</th>
+          </tr>';
+  foreach ($companies as $company) {
+    $name = $company->getElementsByTagName("companyName")->item(0)->nodeValue;
+    $year = $company->getElementsByTagName("yearStart")->item(0)->nodeValue;
+    $tagline = $company->getElementsByTagName("tagline")->item(0)->nodeValue;
+    $branches = $company->getElementsByTagName("totalBranch")->item(0)->nodeValue;
+    $headquarter = $company->getElementsByTagName("headquarter")->item(0)->nodeValue;
+
+    echo '<tr>';
+    echo "<td>" . $name . "</td> ";
+    echo "<td>" . $year . "</td>";
+    echo "<td>" . $tagline . "</td>";
+    echo "<td>" . $branches . "</td>";
+    echo "<td>" . $headquarter . "</td>";
+    echo "</tr>";
+  }
+
+  echo '</table>';
+  ?>
+
 </body>
 
 </html>
