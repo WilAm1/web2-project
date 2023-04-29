7be6123fd135afe16c888b1648746bb9cc5a5097
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-04-29 22:11:56 +0800

    feat: Remove search

--- update.php
+++ update.php
@@ -1,19 +1,47 @@
 <!DOCTYPE html>
+
 <head>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
     <link rel="stylesheet" href="BSIT3EG1G4.css" />
     <title>Update </title>
+    <!-- bootstrap  -->
+    <script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css" />
+    <!--  JQuery and JQueryUI  -->
+    <script src="assets/jquery-3.6.4.min.js"></script>
+    <script src="assets/jquery-ui/jquery-ui.min.js"></script>
+    <link href="assets/jquery-ui.css" rel="stylesheet" type="text/css" />
+    <!-- Own JS  -->
+    <script src="assets/js/updateScript.js"></script>
+    <!-- Own CSS -->
+    <link rel="stylesheet" href="assets/styles.css" />
+
+
+
+
 </head>
 
-<body class="bg-secondary">
-    <div class="container text-center">
-        <h1 class="text-light">Update a Company</h1>
+<body>
+    <nav>
+        <div class="menus mt-5 d-flex justify-content-center nav-box">
+            <a class="nav-link m-2 text-decoration-none" href="index.php">Home</a>
+            <a class="nav-link  m-2 text-decoration-none" href="create.html">Add</a>
+            <a class="nav-link active m-2 text-decoration-none" href="update.php">Edit</a>
+            <a class="nav-link m-2 text-decoration-none" href="delete.php">Delete</a>
+
+
+        </div>
+    </nav>
+
+
+    <div class="container ">
+        <h1 class="text-accent mt-5 text-center">Update a Company</h1>
         <form action="updateProcess.php" method="post">
 
             <div class="formContainer">
 
                 <!-- company name in dropdown -->
-                <div  class="input-group mb-2 w-75 mx-auto">
+                <div class="input-group mb-2 w-75 mx-auto">
                     <label class="input-group-text fw-bold" for="name">Name</label>
                     <select class="form-select" name="name" id="name">
                         <?php
