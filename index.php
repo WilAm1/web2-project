b7fbb0b64124c2a4af17534839b1b7784517d133
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-05-06 18:54:50 +0800

    feat: Add style to create section

--- index.php
+++ index.php
@@ -40,11 +40,11 @@
     <!-- <div class="collapse navbar-collapse" id="navbarText"> -->
     <ul class="navbar-nav mb-2 mb-lg-0 text-white">
       <li class="nav-item">
-        <a class="nav-link active " href="">Home</a>
+        <a class="nav-link active " href="./index.php">Home</a>
         <div class="active-box"></div>
       </li>
       <li class="nav-item ">
-        <a class="nav-link  " href="">Create</a>
+        <a class="nav-link  " href="./create.html">Create</a>
         <div class="active-box"></div>
       </li>
       <li class="nav-item">
